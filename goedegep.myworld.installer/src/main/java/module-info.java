249bc27fa250676be08c0e5c6e9682666d460c04
goedegep <peter.goedegebure@gmail.com>
2022-02-23 20:10:45 +0100

    goedegep.myworld.installer

---
+++ goedegep.myworld.installer/src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module goedegep.myworld.installer {
+  exports goedegep.myworld.installer;
+  
+  requires goedegep.appgen;
+  requires goedegep.appgenfx;
+  requires goedegep.model.properties;
+  requires goedegep.util;
+  requires java.logging;
+  requires javafx.controls;
+  requires transitive javafx.graphics;
+  requires javafx.web;
+  requires javafx.base;
+  requires org.eclipse.emf.ecore;
+}
