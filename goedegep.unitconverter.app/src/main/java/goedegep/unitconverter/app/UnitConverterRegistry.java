05952a04bc562a4dd0a0920092702de6574a8fc2
goedegep <peter.goedegebure@gmail.com>
2022-02-23 20:21:14 +0100

    goedegep.unitconverter.app

---
+++ goedegep.unitconverter.app/src/main/java/goedegep/unitconverter/app/UnitConverterRegistry.java
@@ -0,0 +1,16 @@
+package goedegep.unitconverter.app;
+
+import goedegep.appgen.EMFResource;
+import goedegep.properties.model.PropertyDescriptorGroup;
+
+public class UnitConverterRegistry {
+  public static String author = null;                 // Name of the author of the application.
+  public static String configurationFile = null;      // Name of the file with Configuration data.
+  public static String copyrightMessage = null;       // Copyright message for the application.
+  public static String customPropertiesFile = null;   // Name of the file with custom properties.
+  public static String dataDirectory = null;          // Directory where all data files are stored.
+  public static String shortProductInfo = null;       // Short description of this application.
+  public static String version = null;                // Current software version.
+  public static boolean developmentMode = false;      // Voor extra functies tijdens ontwikkeling
+  public static EMFResource<PropertyDescriptorGroup> propertyDescriptorsResource = null;
+}
