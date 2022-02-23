dac9cf61fbcbdb263342e05f8ff4a7f2d9d8977b
goedegep <peter.goedegebure@gmail.com>
2022-02-23 20:25:57 +0100

    tim.prune

---
+++ tim.prune/src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module tim.prune {
+  exports tim.prune;
+  exports tim.prune.gui;
+  
+  requires java.desktop;
+  requires javafx.graphics;
+  requires j3d.vecmath;
+  requires j3d.core;
+  requires j3d.utils;
+
+}
