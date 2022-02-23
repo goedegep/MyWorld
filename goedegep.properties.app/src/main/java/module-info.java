73e3a0738aadcb02a31a48935030ff1fa742d74e
goedegep <peter.goedegebure@gmail.com>
2022-02-23 20:15:30 +0100

    goedegep.properties.app

---
+++ goedegep.properties.app/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module goedegep.properties.app {
+  exports goedegep.properties.app;
+  exports goedegep.properties.app.guifx;
+
+  requires goedegep.appgen;
+  requires transitive goedegep.appgenfx;
+  requires transitive goedegep.model.properties;
+  requires goedegep.util;
+  requires java.desktop;
+  requires java.logging;
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.graphics;
+  requires org.eclipse.emf.common;
+  requires org.eclipse.emf.ecore;
+  requires junit;
+}
